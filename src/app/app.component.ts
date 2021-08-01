c60feecede20246bc4cd0f8634019b840099e8dd
Shayan <shayansaif@hotmail.com>
2021-08-01 19:04:13 -0400

    Use of local storage to persist theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ContactForm';
-  darkMode = false;
-  toggleLabel: string = 'Dark mode';
+  mode = {
+    dark: false,
+    label: 'Light'
+  }
 
-  toggleDark(): void {
-    this.darkMode = !this.darkMode;
-    if (this.toggleLabel === 'Dark mode') {
-      this.toggleLabel = 'Light mode';
+  ngOnInit(): void {
+
+    const mode = window.localStorage.getItem("mode");
+    if (mode) {
+      this.setTheme(mode);
     } else {
-      this.toggleLabel = 'Dark mode';
+      window.localStorage.setItem("mode", "Light Mode")
+    }
+
+  }
+
+  setTheme(value: string): void {
+    if (value === "Dark Mode") {
+      this.mode = {
+        dark: true,
+        label: "Light Mode"
+      }
+    } else {
+      this.mode = {
+        dark: false,
+        label: "Dark Mode"
+      }
     }
   }
+
+  toggleDark(): void {
+    if (this.mode.dark) {
+      window.localStorage.setItem("mode", "Light Mode")
+      this.setTheme("Light Mode")
+    } else {
+      window.localStorage.setItem("mode", "Dark Mode")
+      this.setTheme("Dark Mode")
+    }
+  }
+
 }
